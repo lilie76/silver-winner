af6833e1e728916f8363c6f31140d2b3013816fd
unknown <lilie7679@gmail.com>
2026-02-27 11:27:37 -0300

    Config para deploy en Render

--- app.py
+++ app.py
@@ -57,3 +57,8 @@ def crear_solicitud():
 if __name__ == "__main__":
     print("🚀 Salvabioverde iniciado en http://127.0.0.1:5000")
     app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
